f4d78271e7a8b651d402975eae8594ce1b14a05c
onio1100 <nonio1100n@gmail.com>
2023-05-03 23:50:47 +0200

    S

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,7 @@
-import { useState } from "react"
-
  export default function App() {
-  const [count, setCount] = useState(1);
-
-  function countControl() {
-    setCount((prevCount) => {
-      if(prevCount + 1 > 9){
-        return 1;
-      }else{
-        return prevCount + 1;
-      }
-    })
-  }
-
   return (
     <>
       <div className="app">
-        <p className="button" onClick={() => {countControl}}>{count}</p>
       </div>
     </>
   )
