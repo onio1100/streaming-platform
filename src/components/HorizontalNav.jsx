bef850f7fa13f4c71e321c66af7e17482d48f897
onio1100 <nonio1100n@gmail.com>
2023-07-08 23:44:40 +0200

    progres save

--- src/components/HorizontalNav.jsx
+++ src/components/HorizontalNav.jsx
@@ -1,7 +1,17 @@
 export default function HorizontalNav() {
     return(
         <nav className="hnav">
-            
+            <div className="hnav__left">
+                <p className="hnav__left__logo">Blob</p>
+                <p className="hnav__left__browse">Browse</p>
+                <div className="hnav__left__more"><span class="material-symbols-outlined">more_vert</span></div>
+            </div>
+            <div className="hnav__middle">
+
+            </div>
+            <div className="hnav__right">
+
+            </div>
         </nav>
     )
 }
