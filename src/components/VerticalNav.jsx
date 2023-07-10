1a9ce0d9b2e03b04d04a008e63e642f78329aa0b
onio1100 <nonio1100n@gmail.com>
2023-07-10 22:58:20 +0200

    progres save

--- src/components/VerticalNav.jsx
+++ src/components/VerticalNav.jsx
@@ -1,3 +1,5 @@
+import avatar from "../assets/defult avatar.png";
+// temp 
 import { useState } from "react"
 
 export default function VerticalNav() {
@@ -11,9 +13,28 @@ export default function VerticalNav() {
 
     return(
         <div className={smallNav ? "vnav vnav--small" : "vnav"}>
-        <div className="vnav__wraper--title">
-            <p className="vnav__title">{smallNav ? "" : "For you"}</p>
+        <div className="vnav__wraper--top">
+            <p className="vnav__title--top">{smallNav ? "" : "For you"}</p>
             <div className="vnav__switch" onClick={switchNav}><span class="material-symbols-outlined">{smallNav ? "login" : "logout"}</span></div>
+        </div>
+        <div className="vnav__wraper--followed">
+            <p className="vnav__title--followed">{smallNav ? <span class="material-symbols-outlined">favorite</span> : "FOLLOWED CHANNELS:"}</p>
+            <div className="vnav__wraper--channel">
+                <img className="vnav__img--channel" src={avatar} alt="avatar" />
+                {smallNav ? "" : (<>
+                    <div className="vnav__wraper--text">
+                        <p className="vnav__text--nick">YoungMulti</p>
+                        <p className="vnav__text--category">Just Chatting</p>
+                    </div>
+                    <p className="vnav__wraper--count">
+                        <div className="vnav__dot"></div>
+                        <p className="vnav__text--count">10,8 k</p>
+                    </p>
+                </>)}   
+            </div>
+        </div>
+        <div className="vnav__wraper--recomended">
+            <p className="vnav__title--recomended">{smallNav ? <span class="material-symbols-outlined">videocam</span> : "RECOMENDED CHANNELS:"}</p>
         </div>
         </div>
     )
