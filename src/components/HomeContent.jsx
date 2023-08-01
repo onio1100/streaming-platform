96cbd01c0f67a366b6d4614ad13116b3ede53d90
onio1100 <nonio1100n@gmail.com>
2023-08-01 22:54:56 +0200

    s

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -1,9 +1,54 @@
+import streamrs from "../data/stremars.json"
 import { Link } from "react-router-dom"
 import avatar from "../assets/defult avatar.png"
 export default function HomeContent(){
+    const randomaizedStremers = fyShuffle(streamrs);
+
+    function fyShuffle(arr) {
+        let i = arr.length;
+        while (--i > 0) {
+          let randIndex = Math.floor(Math.random() * (i + 1));
+          [arr[randIndex], arr[i]] = [arr[i], arr[randIndex]];
+        }
+        return arr;
+      }
+
+    function creatFeed(){
+        let stremerIndex = 0;
+        let JsxObjectsArr = [];
+        for(let row = 0;row < 16; row++){
+            let rowObj = [];
+            for(let column = 0; column < 6; column++){
+                let streamer = randomaizedStremers[stremerIndex];
+                rowObj.push(
+                    <Link to={"/" + streamer.user} className="content__tile">
+                        <img className="ctile__cover"></img>
+                        <div className="ctile__wraper--big">
+                            <img className="ctile__avatar" src={avatar} alt="user avatar"></img>
+                            <div className="ctile__wraper--small">
+                                <p className="ctile__title">{streamer.title}</p>
+                                <p className="ctile__nick">{streamer.user}</p>
+                                <p className="ctile__game">{streamer.game}</p>
+                            </div>
+                        </div>
+                    </Link>
+                )
+                stremerIndex++;
+            }
+
+            JsxObjectsArr.push(<div className="content__line">{rowObj}</div>)
+
+            if(row < 15){
+                JsxObjectsArr.push(<div className="content__bar"></div>)
+            }
+        }
+        return JsxObjectsArr;
+    }
+
     return(
-        <div className="content__wraper">    
-            <p className="content__title">Recomended to you</p>
+        <div className="content__wraper">
+            {creatFeed()}    
+            {/* <p className="content__title">Recomended to you</p>
             <div className="content__line">
 
                 <Link to="super strimer 1" className="content__tile">
@@ -76,7 +121,7 @@ export default function HomeContent(){
 
             </div>
             <div className="content__bar"></div>
-            
+             */}
         </div>
     )
 }
