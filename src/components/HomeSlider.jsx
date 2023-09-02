bb6205f2aa99efbcbcc4fe7784188bb7df348d48
onio1100 <nonio1100n@gmail.com>
2023-09-02 23:35:32 +0200

    s

--- src/components/HomeSlider.jsx
+++ src/components/HomeSlider.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function HomeSlider({streamers}) {
     const [sliderPosition, setSliderPositon] = useState(["back","left--2","left--1","center","right--1","right--2"]);
 
-    function mvoeSlider(direction = false){ // direction(false = right, true = left)
+    function moveSlider(direction = false){ // direction(false = right, true = left)
         setSliderPositon((prevPosition) => {
             if(direction){
                 return [prevPosition[5],prevPosition[0],prevPosition[1],prevPosition[2],prevPosition[3],prevPosition[4]];
@@ -14,18 +14,31 @@ export default function HomeSlider({streamers}) {
         })
     }
 
+    function creatSliderElements(){
+        const sliderElements = streamers.map((streamer, id) => {
+            return(
+                <Link to={streamer.user} key={id} className={"slider__tile" + ` slider__tile--${sliderPosition[id]}`}>
+                    <img src={streamer.cover} alt="cover" className="slider__img" />
+                    <div className="slider__wraper">
+                        <p className="slider__username">{streamer.user}</p>
+                        <div className="slider__count">
+                            <div className="slider__dot"></div>
+                            <p className="slider__viewers">{streamer.viewers}</p>
+                        </div>
+                    </div>
+                </Link>
+            )
+        })
+        return sliderElements;
+    }
+
     return(
         <div className="slider">
-            <div className="slider__arrow--left" onClick={() => mvoeSlider(true)}><span className="material-symbols-outlined">chevron_left</span></div>
+            <div className="slider__arrow--left" onClick={() => moveSlider(true)}><span className="material-symbols-outlined">chevron_left</span></div>
             <div className="slider__main">
-                <Link to={streamers[0].user} style={{background: "red"}} className={"slider__tile" + ` slider__tile--${sliderPosition[0]}`}><img src={streamers[0].cover} alt="cover"></img></Link>
-                <Link to={streamers[1].user} style={{background: "blue"}} className={"slider__tile" + ` slider__tile--${sliderPosition[1]}`}><img src={streamers[1].cover} alt="cover"></img></Link>
-                <Link to={streamers[2].user} style={{background: "green"}} className={"slider__tile" + ` slider__tile--${sliderPosition[2]}`}><img src={streamers[2].cover} alt="cover"></img></Link>
-                <Link to={streamers[3].user} style={{background: "pink"}} className={"slider__tile" + ` slider__tile--${sliderPosition[3]}`}><img src={streamers[3].cover} alt="cover"></img></Link>
-                <Link to={streamers[4].user} style={{background: "orange"}} className={"slider__tile" + ` slider__tile--${sliderPosition[4]}`}><img src={streamers[4].cover} alt="cover"></img></Link>
-                <Link to={streamers[5].user} style={{background: "white"}} className={"slider__tile" + ` slider__tile--${sliderPosition[5]}`}><img src={streamers[5 ].cover} alt="cover"></img></Link>
+                {creatSliderElements()}
             </div>
-            <div className="slider__arrow--right" onClick={() => mvoeSlider()}><span className="material-symbols-outlined">chevron_right</span></div>
+            <div className="slider__arrow--right" onClick={() => moveSlider()}><span className="material-symbols-outlined">chevron_right</span></div>
         </div>
     )
 }
